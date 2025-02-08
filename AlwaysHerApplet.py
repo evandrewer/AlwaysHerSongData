ab8e31eeca038db78d2b1dbe2fa75eae353bc25a
evandrewer <etarocks@gmail.com>
2025-02-07 18:59:26 -0700

    working! doin some tests

--- AlwaysHerApplet.py
+++ AlwaysHerApplet.py
@@ -52,6 +52,7 @@ song_titles = ['Silhouette', 'In the Beginning', 'Airport Girl', 'Mr. Nice Guy',
                'In the Beginning - Acoustic', 'Airport Girl - Acoustic', 'Timeless']
 selected_songs = st.sidebar.multiselect(
     "Select Songs", options=song_titles, default=song_titles)
+
 data_by_song = song_data[song_data['song'].isin(selected_songs)]
 
 
@@ -64,10 +65,11 @@ grand_total = total_streams_per_song['streams'].sum()
 
 
 # For tab1 col2
-data_by_song['cumulative_streams'] = data_by_song.groupby('song')['streams'].cumsum() 
+#data_by_song['cumulative_streams'] = data_by_song.groupby('song')['streams'].cumsum() 
 
 def calculate_growth_rate(group):
-    group['growth_rate'] = group['cumulative_streams'].pct_change(periods=21) * 100
+#    group['growth_rate'] = group['cumulative_streams'].pct_change(periods=21) * 100   #(Cumulative)
+    group['growth_rate'] = group['streams'].pct_change(periods=21) * 100              #(Normal)
     return group
 
 data_by_song = data_by_song.groupby('song', group_keys=False).apply(calculate_growth_rate)
