c382b34ad6a54897114f930d4e06b1353db7406e
evandrewer <etarocks@gmail.com>
2025-03-10 23:32:03 -0600

    final tweaks 3

--- AlwaysHerApplet.py
+++ AlwaysHerApplet.py
@@ -617,7 +617,7 @@ with tab1:
 
             # shrink legend and add margins
             fig2.update_layout(
-                margin=dict(t=35, b=35, l=35, r=35)
+                margin=dict(t=35, b=35, l=35, r=0)
             )
 
             # Display labels directly on the pie chart
